74940419b15053df8b029014b841693bafd60cec
brendan-mccaffrey <brendanchristophermccaffrey@gmail.com>
2022-12-26 18:08:02 -0500

    hey

--- backtest/backtest.py
+++ backtest/backtest.py
@@ -10,25 +10,47 @@ mymarkets = [
     "COMP-USD",
 ]
 
-df = pd.read_pickle("data/dydx/funding/combined.pkl")
+shorts = mymarkets[1:]
+long = "ETH-USD"
 
+df = pd.read_pickle("../data/dydx/funding/combined.pkl")
 first = df.iloc[0]
 
 
 def compute_value_equal_short(row):
     """Compute value of equal weight short strategy"""
 
+    print(shorts)
+    # print(row.name, " | ", row)
+
     weight_per_item = 1 / (len(mymarkets) - 1)
     if row.name == first.name:
-        value = 500000
+        print("First row, skipping...")
     else:
-        # get previous row value
-        prev = row.name - pd.Timedelta(hours=1)
-        print(df.loc[prev])
+        prev = df.loc[row.name - pd.Timedelta(hours=1)]
+
+        short_day_factor = 0
+        for short in shorts:
+            # hr change = (today - yesterday) / yesterday
+            hr_change = (row[short + " price"] - float(prev[short + " price"])) / float(
+                prev[short + " price"]
+            )
+            # negate change and factor funding
+            hr_short_perf = -hr_change * (1 + row[short + " rate"])
+            short_day_factor += (1 + hr_short_perf) * weight_per_item
+
+        row["shorts_value"] = prev["shorts_value"] * short_day_factor
+
+        eth_hr_change = (row[long + " price"] - float(prev[long + " price"])) / float(
+            prev[long + " price"]
+        )
+        eth_hr_perf = eth_hr_change * (1 - row[long + " rate"])
+        row["longs_value"] = prev["longs_value"] * eth_hr_perf
+
         print()
-        # value = df.loc[prev]["shorts_value"] * 0
-        # if df.loc[prev] == 0:
-        #     print("AH")
+    # value = df.loc[prev]["shorts_value"] * 0
+    # if df.loc[prev] == 0:
+    #     print("AH")
 
     # print("They are equal?", row.name == first.name)
 
@@ -40,14 +62,19 @@ def compute_value_equal_short(row):
 def backtest_equal_short(total_size=1e7, start_date="2022-08-01"):
     """Backtest equal weight short strategy"""
 
-    df = pd.read_pickle("data/dydx/funding/combined.pkl")
+    df = pd.read_pickle("../data/dydx/funding/combined.pkl")
 
     # add a new column of 0s to the dataframe
     df["shorts_value"] = 500000
+    df["longs_value"] = 500000
 
-    print(df.head())
+    print(df.head(3))
 
-    df.iloc[:5].apply(compute_value_equal_short, axis=1)
+    df = df.astype(float)
+
+    df.iloc[:3].apply(compute_value_equal_short, axis=1)
+
+    print(df.head(3))
 
     # hello
 
